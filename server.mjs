0dc2b4d0326a664f02b7c697cd09c2069e1e5b07
GalPaz <galpaz2210@gmail.com>
2024-08-18 20:00:10 +0300

    first commit4

--- server.mjs
+++ server.mjs
@@ -41,9 +41,7 @@ const buildAggregationPipeline = (queryEmbedding, filters, siteId) => {
 
     const matchStage = {};
 
-    if (filters.category) {
-        matchStage.category = { $regex: filters.category, $options: "i" };
-    }
+  
     
 
     if (filters.minPrice && filters.maxPrice) {
