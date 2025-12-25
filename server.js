6f64c4bbf74f029008fddeeb05c71d537f986995
GalPaz22 <galpaz2210@gmail.com>
2025-12-25 13:24:43 +0200

    this should work3

--- server.js
+++ server.js
@@ -463,7 +463,7 @@ app.post("/queries", async (req, res) => {
     const db = client.db(dbName);
     const queriesCollection = db.collection("queries");
 
-    const queries = await queriesCollection.find({}).toArray();
+    const queries = await queriesCollection.find({}).limit(100).toArray();
 
     return res.status(200).json({ queries });
   } catch (error) {
